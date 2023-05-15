5f65d4f00a4f70de8e55a0a1d8e4e1c341732439
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-05-16 01:03:45 +0200

    add Redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,32 @@
-import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { ContactForm } from './contactForm/ContactForm';
 import { ContactList } from './contactList/ContactList';
 import { Filter } from './filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { setContacts, setFilter } from 'redux/contactsSlice';
 
 export const App = () => {
 
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('contacts')) ?? [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermine Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
-  );
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(state => state.contactData.contacts)
+  const filter = useSelector(state => state.contactData.filter)
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const dispatch = useDispatch();
+
+  console.log('contacts :>> ', contacts);
+  // const [contacts, setContacts] = useState(
+  //   JSON.parse(localStorage.getItem('contacts')) ?? [
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermine Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ]
+  // );
+  // const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
 
   function addNewContact(data) {
     const newContact = {
@@ -39,7 +46,9 @@ export const App = () => {
       return;
     }
 
-    setContacts(prev => [...prev, newContact]);
+    // dispatch(setContacts(prev => [...prev, newContact]));
+    dispatch(setContacts([...contacts, newContact]));
+
   }
 
   function getFilteredContacts() {
@@ -50,11 +59,13 @@ export const App = () => {
   }
 
   function onChangeFilter({ currentTarget: { value } }) {
-    setFilter(value);
+    dispatch(setFilter(value));
   }
 
   function deleteContact(contactId) {
-    setContacts(prev => prev.filter(({ id }) => id !== contactId));
+    // dispatch(setContacts(prev => prev.filter(({ id }) => id !== contactId)));
+    dispatch(setContacts(contacts.filter(({ id }) => id !== contactId)));
+
   }
 
   return (
